3440f7210b98b6b03a0371db82d2a67920c02b79
cpusch <conpusch@yahoo.com>
2023-02-11 18:56:35 +0000

    updated to return data from database

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
+from SQLiteExample import ExecuteQuery
 
 # config stuff to expose to frontend application
 middlewares = [
@@ -22,3 +23,9 @@ async def root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id, "description": f"{item_id} is an item"}
+
+# returns all rows with given patient ID using SQLiteExample.py
+@app.get("/patient/{patient_id}")
+async def run_query(patient_id: str):
+    query = f'select * from test where PATIENT="{patient_id}"'
+    return ExecuteQuery(query=query)
