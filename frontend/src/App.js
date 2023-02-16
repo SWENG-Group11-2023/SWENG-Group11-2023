e104ecbe12b837ef24fa5383e509beec9dea7812
oisin <gavrila@tcd.ie>
2023-02-16 01:11:03 +0000

    created  basic searchbar that takes in input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import {useState} from 'react';
+
+const SearchBar = () => {
+  const [message, setMessage] = useState('');
+
+  const handleKeyDown = event => {
+    console.log(event.key);
+
+    if (event.key === 'Enter') {
+      
+      event.preventDefault();
+      console.log(message);
+      // console.log(event.target.value)
+
+      console.log('User pressed Enter ✅');
+      console.log("Typed Word :" + message);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <p >Search bar!</p>
+      <input
+        type="text"
+        id="message"
+        name="message"
+        value={message}
+        onChange={event => setMessage(event.target.value)}
+        onKeyDown={handleKeyDown}
+        message =""
+      />
+      <p>You typed: {message}</p>
+     
+     
     </div>
+    
   );
-}
+};
 
-export default App;
+
+export default SearchBar;
