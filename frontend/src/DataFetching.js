9269d3be21c3821cd4fa7d79aed37d742b8c7c95
cpusch <conpusch@yahoo.com>
2023-02-20 18:57:44 +0000

    changed order of imports

--- frontend/src/DataFetching.js
+++ frontend/src/DataFetching.js
@@ -1,34 +1,34 @@
 import React from 'react';
+import { useState, useEffect } from "react";
 import axios from 'axios';
-import {  useState , useEffect} from "react";
 
 
 var data = ({})
 
 function DataFetching() {
-  const [dataPionts, setDataPionts] = useState({})
+    const [dataPionts, setDataPionts] = useState({})
     const [id, setId] = useState({})
 
 
-    useEffect(()=>{
-      const getData = async () => {
-         data = await axios.get(`http://127.0.0.1:8000/patient/${id}`)
-        setDataPionts(data)
-      };
-      getData();
-    },[id] )
+    useEffect(() => {
+        const getData = async () => {
+            data = await axios.get(`http://127.0.0.1:8000/patient/${id}`)
+            setDataPionts(data)
+        };
+        getData();
+    }, [id])
 
-  console.log("data: ",dataPionts)
+    console.log("data: ", dataPionts)
 
-  return (
-    <div>
-    <input 
-    type = "text" 
-    value = {id} 
-    onChange={e => setId(e.target.value)}
-    />
-      {dataPionts.data ? <h2>patient: {dataPionts.data}</h2> : null}
-    </div>
-  )
+    return (
+        <div>
+            <input
+                type="text"
+                value={id}
+                onChange={e => setId(e.target.value)}
+            />
+            {dataPionts.data ? <h2>patient: {dataPionts.data}</h2> : null}
+        </div>
+    )
 }
-export  {DataFetching};
+export { DataFetching };
