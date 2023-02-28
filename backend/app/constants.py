4ad61dd8f01d2c9090bba860c06a93e6fb38c042
Andrew Furey <anfurey@tcd.ie>
2023-02-28 07:52:14 +0000

    added DB_TABLE_NAME

--- backend/app/constants.py
+++ backend/app/constants.py
@@ -1,3 +1,4 @@
 # constants to be used throughout the project to have easy change of file names etc
 DB_PATH = './production.db'
 DATA_CSV = './data/observations_clean.csv'
+DB_TABLE_NAME = 'main_table'
