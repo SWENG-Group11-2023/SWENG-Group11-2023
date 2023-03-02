cb23e500c18671749405c9be7e82475c27d0283d
Luke-Barry <lukebarry100@gmail.com>
2023-03-02 09:44:00 +0000

    Added Navbar and messed around with some ideas for colour schemes.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Analytics from './pages/Analytics';
 import SideBar from './components/SideBar';
+import Navbar from './components/Navbar';
+import React from 'react';
 
 const App = () => {
   return (
     <BrowserRouter>
+    <React.Fragment>
+      <Navbar/>
+    </React.Fragment>
     <SideBar>
     <Routes>
       <Route path ="/"element={<Dashboard/>}/>
