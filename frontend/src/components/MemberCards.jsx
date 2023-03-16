696609b960fc706137d14bba5615f9db6087241d
Aj Pakulyte <pakulyta@tcd.ie>
2023-03-16 18:37:41 +0000

    Filled in info, button colour

--- frontend/src/components/MemberCards.jsx
+++ frontend/src/components/MemberCards.jsx
@@ -19,10 +19,125 @@ export default function App() {
             <MDBCardText>
               3rd Year - Intergrated Computer Science
             </MDBCardText>
-            <MDBBtn href='https://github.com/pakulyta'>Github</MDBBtn>
+            <div class="text-center">
+            <MDBBtn style={{ backgroundColor: '#295c38' }} href='https://github.com/pakulyta'>Github</MDBBtn>
+            </div>
           </MDBCardBody>
         </MDBCard>
       </MDBCol>
+
+      <MDBCol>
+        <MDBCard>
+          <MDBCardBody>
+            <MDBCardTitle>Constantin Pusch</MDBCardTitle>
+            <MDBCardText>
+              3rd Year - Computer Science & Business
+            </MDBCardText>
+            <div class="text-center">
+            <MDBBtn style={{ backgroundColor: '#295c38' }} href='https://github.com/cpusch'>Github</MDBBtn>
+            </div>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+
+      <MDBCol>
+        <MDBCard>
+          <MDBCardBody>
+            <MDBCardTitle>Kevin Morley</MDBCardTitle>
+            <MDBCardText>
+              3rd Year - Intergrated Computer Science
+            </MDBCardText>
+            <div class="text-center">
+            <MDBBtn style={{ backgroundColor: '#295c38' }} href='https://github.com/k-e-v-i-n17'>Github</MDBBtn>
+            </div>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+
+      <MDBCol>
+        <MDBCard>
+          <MDBCardBody>
+            <MDBCardTitle>Andrew Furey</MDBCardTitle>
+            <MDBCardText>
+              2nd Year - 
+            </MDBCardText>
+            <div class="text-center">
+            <MDBBtn style={{ backgroundColor: '#295c38' }} href='https://github.com/anfurey'>Github</MDBBtn>
+            </div>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+
+      <MDBCol>
+        <MDBCard>
+          <MDBCardBody>
+            <MDBCardTitle>Anthony Oisin Gavril </MDBCardTitle>
+            <MDBCardText>
+              2nd Year - 
+            </MDBCardText>
+            <div class="text-center">
+            <MDBBtn style={{ backgroundColor: '#295c38' }} href='https://github.com/onion-mints'>Github</MDBBtn>
+            </div>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+
+      <MDBCol>
+        <MDBCard>
+          <MDBCardBody>
+            <MDBCardTitle>Isobel Radford-Dodd</MDBCardTitle>
+            <MDBCardText>
+              2nd Year - 
+            </MDBCardText>
+            <div class="text-center">
+            <MDBBtn style={{ backgroundColor: '#295c38' }} href='https://github.com/IsobelRadford-Dodd'>Github</MDBBtn>
+            </div>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+
+      <MDBCol>
+        <MDBCard>
+          <MDBCardBody>
+            <MDBCardTitle>Luke Barry</MDBCardTitle>
+            <MDBCardText>
+              2nd Year - 
+            </MDBCardText>
+            <div class="text-center">
+            <MDBBtn style={{ backgroundColor: '#295c38' }} href='https://github.com/Luke-Barry'>Github</MDBBtn>
+            </div>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+
+      <MDBCol>
+        <MDBCard>
+          <MDBCardBody>
+            <MDBCardTitle>Minghim Foun</MDBCardTitle>
+            <MDBCardText>
+              2nd Year - 
+            </MDBCardText>
+            <div class="text-center">
+            <MDBBtn style={{ backgroundColor: '#295c38' }} href='https://github.com/OscarFoun'>Github</MDBBtn>
+            </div>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+
+      <MDBCol>
+        <MDBCard>
+          <MDBCardBody>
+            <MDBCardTitle>Samuel Forster</MDBCardTitle>
+            <MDBCardText>
+              2nd Year - 
+            </MDBCardText>
+            <div class="text-center">
+            <MDBBtn style={{ backgroundColor: '#295c38' }} href='https://github.com/SamPFF'>Github</MDBBtn>
+            </div>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+
     </MDBRow>
   );
 }
