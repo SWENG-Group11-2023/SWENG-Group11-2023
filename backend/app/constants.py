4c330a3d7187383aa051e15fecab3149eb71008d
cpusch <conpusch@yahoo.com>
2023-03-19 18:47:15 +0000

    added new location of DATA_CSV and DATA_URL

--- backend/app/constants.py
+++ backend/app/constants.py
@@ -1,9 +1,10 @@
 # constants to be used throughout the project to have easy change of file names etc
 DB_PATH = './production.db'
-DATA_CSV = './data/observations_clean.csv'
+DATA_CSV = './csv/observations.csv'
 DESCRIPTIONS = './descriptions.json'
 DB_TABLE_NAME = 'main_table'
 PATIENT_ID_COLUMN = 1
 VALUE_COLUMN = 5
 UNITS_COLUMN = 6
 TYPE_COLUMN = 7
+DATA_URL = "https://synthetichealth.github.io/synthea-sample-data/downloads/synthea_sample_data_csv_apr2020.zip"
