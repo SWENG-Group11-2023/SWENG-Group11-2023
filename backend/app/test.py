67f233945f0fec00d0028b557ef607fb281e4754
cpusch <conpusch@yahoo.com>
2023-03-19 19:49:50 +0000

    fixed tests for new date format

--- backend/app/test.py
+++ backend/app/test.py
@@ -7,23 +7,23 @@ from nltk.corpus import wordnet
 
 def test_execute_query():
     test_result = execute_query(f'select * from {DB_TABLE_NAME} where PATIENT="1d604da9-9a81-4ba9-80c2-de3375d59b40" limit 1')
-    expected_result = [('2011-07-28 15:02:18', '1d604da9-9a81-4ba9-80c2-de3375d59b40', 'b85c339a-6076-43ed-b9d0-9cf013dec49d', 
+    expected_result = [('2011-07-28T15:02:18Z', '1d604da9-9a81-4ba9-80c2-de3375d59b40', 'b85c339a-6076-43ed-b9d0-9cf013dec49d', 
                         '8302-2', 'Body Height', '181.0', 'cm', 'numeric')]
     assert test_result == expected_result, f"Got wrong result, expected is: {expected_result}, actual is {test_result}"
 
     test_result = execute_query(f'select * from {DB_TABLE_NAME} where DESCRIPTION="Systolic Blood Pressure" limit 1')
-    expected_result = [('2012-01-23 17:45:28', '034e9e3b-2def-4559-bb2a-7850888ae060', 'e88bc3a9-007c-405e-aabc-792a38f4aa2b', 
+    expected_result = [('2012-01-23T17:45:28Z', '034e9e3b-2def-4559-bb2a-7850888ae060', 'e88bc3a9-007c-405e-aabc-792a38f4aa2b', 
                         '8480-6', 'Systolic Blood Pressure', '119.0', 'mm[Hg]', 'numeric')]
     assert test_result == expected_result, f"Got wrong result, expected is: {expected_result}, actual is {test_result}"
 
     test_result = execute_query(f'select * from {DB_TABLE_NAME} where TYPE="numeric" limit 5')
-    expected_result = [('2012-01-23 17:45:28', '034e9e3b-2def-4559-bb2a-7850888ae060', 'e88bc3a9-007c-405e-aabc-792a38f4aa2b', 
-                        '8302-2', 'Body Height', '193.3', 'cm', 'numeric'), ('2012-01-23 17:45:28', '034e9e3b-2def-4559-bb2a-7850888ae060', 
+    expected_result = [('2012-01-23T17:45:28Z', '034e9e3b-2def-4559-bb2a-7850888ae060', 'e88bc3a9-007c-405e-aabc-792a38f4aa2b', 
+                        '8302-2', 'Body Height', '193.3', 'cm', 'numeric'), ('2012-01-23T17:45:28Z', '034e9e3b-2def-4559-bb2a-7850888ae060', 
                         'e88bc3a9-007c-405e-aabc-792a38f4aa2b', '72514-3', 'Pain severity - 0-10 verbal numeric rating [Score] - Reported',
-                        '2.0', '{score}', 'numeric'), ('2012-01-23 17:45:28', '034e9e3b-2def-4559-bb2a-7850888ae060',
-                        'e88bc3a9-007c-405e-aabc-792a38f4aa2b', '29463-7', 'Body Weight', '87.8', 'kg', 'numeric'), ('2012-01-23 17:45:28',
+                        '2.0', '{score}', 'numeric'), ('2012-01-23T17:45:28Z', '034e9e3b-2def-4559-bb2a-7850888ae060',
+                        'e88bc3a9-007c-405e-aabc-792a38f4aa2b', '29463-7', 'Body Weight', '87.8', 'kg', 'numeric'), ('2012-01-23T17:45:28Z',
                         '034e9e3b-2def-4559-bb2a-7850888ae060', 'e88bc3a9-007c-405e-aabc-792a38f4aa2b', '39156-5', 'Body Mass Index', '23.5',
-                        'kg/m2', 'numeric'), ('2012-01-23 17:45:28', '034e9e3b-2def-4559-bb2a-7850888ae060', 'e88bc3a9-007c-405e-aabc-792a38f4aa2b',
+                        'kg/m2', 'numeric'), ('2012-01-23T17:45:28Z', '034e9e3b-2def-4559-bb2a-7850888ae060', 'e88bc3a9-007c-405e-aabc-792a38f4aa2b',
                         '8462-4', 'Diastolic Blood Pressure', '82.0', 'mm[Hg]', 'numeric')]
     assert test_result == expected_result, f"Got wrong result, expected is: {expected_result}, actual is {test_result}"
 
@@ -43,26 +43,26 @@ def test_process_query():
 
 
 def test_format_rows_for_graphing():
-    test_result = format_rows_for_graphing([('2011-07-28 15:02:18', '1d604da9-9a81-4ba9-80c2-de3375d59b40', 'b85c339a-6076-43ed-b9d0-9cf013dec49d',
+    test_result = format_rows_for_graphing([('2011-07-28T15:02:18Z', '1d604da9-9a81-4ba9-80c2-de3375d59b40', 'b85c339a-6076-43ed-b9d0-9cf013dec49d',
                                              '8302-2', 'Body Height', '181.0', 'cm', 'numeric')])
     
     expected_result = [{'name': '1d604da9-9a81-4ba9-80c2-de3375d59b40', 'value': '181.0'}]
     assert test_result == expected_result, f"Got wrong result, expected is: {expected_result}, actual is {test_result}"   
 
 
-    test_result = format_rows_for_graphing([('2012-01-23 17:45:28', '034e9e3b-2def-4559-bb2a-7850888ae060', 'e88bc3a9-007c-405e-aabc-792a38f4aa2b',
+    test_result = format_rows_for_graphing([('2012-01-23T17:45:28Z', '034e9e3b-2def-4559-bb2a-7850888ae060', 'e88bc3a9-007c-405e-aabc-792a38f4aa2b',
                                              '8480-6', 'Systolic Blood Pressure', '119.0', 'mm[Hg]', 'numeric')])
     
     expected_result = [{'name': '034e9e3b-2def-4559-bb2a-7850888ae060', 'value': '119.0',}]
     assert test_result == expected_result, f"Got wrong result, expected is: {expected_result}, actual is {test_result}"   
 
-    test_result = format_rows_for_graphing([('2012-01-23 17:45:28', '034e9e3b-2def-4559-bb2a-7850888ae060', 'e88bc3a9-007c-405e-aabc-792a38f4aa2b', 
+    test_result = format_rows_for_graphing([('2012-01-23T17:45:28Z', '034e9e3b-2def-4559-bb2a-7850888ae060', 'e88bc3a9-007c-405e-aabc-792a38f4aa2b', 
                         '8302-2', 'Body Height', '193.3', 'cm', 'numeric'), ('2012-01-23 17:45:28', '034e9e3b-2def-4559-bb2a-7850888ae060', 
                         'e88bc3a9-007c-405e-aabc-792a38f4aa2b', '72514-3', 'Pain severity - 0-10 verbal numeric rating [Score] - Reported',
                         '2.0', '{score}', 'numeric'), ('2012-01-23 17:45:28', '034e9e3b-2def-4559-bb2a-7850888ae060',
                         'e88bc3a9-007c-405e-aabc-792a38f4aa2b', '29463-7', 'Body Weight', '87.8', 'kg', 'numeric'), ('2012-01-23 17:45:28',
                         '034e9e3b-2def-4559-bb2a-7850888ae060', 'e88bc3a9-007c-405e-aabc-792a38f4aa2b', '39156-5', 'Body Mass Index', '23.5',
-                        'kg/m2', 'numeric'), ('2012-01-23 17:45:28', '034e9e3b-2def-4559-bb2a-7850888ae060', 'e88bc3a9-007c-405e-aabc-792a38f4aa2b',
+                        'kg/m2', 'numeric'), ('2012-01-23T17:45:28Z', '034e9e3b-2def-4559-bb2a-7850888ae060', 'e88bc3a9-007c-405e-aabc-792a38f4aa2b',
                         '8462-4', 'Diastolic Blood Pressure', '82.0', 'mm[Hg]', 'numeric')])
     
     expected_result = [{'name': '034e9e3b-2def-4559-bb2a-7850888ae060', 'value': '193.3'}, {'name': '034e9e3b-2def-4559-bb2a-7850888ae060',
@@ -115,6 +115,7 @@ def test_remove_stopwords():
 
 
 if __name__ == "__main__":
+    create_db()
     test_execute_query()
     test_process_query()
     test_format_rows_for_graphing()
