1337edb7bc9967a05ddbd16160cee24882566747
Aj Pakulyte <pakulyta@tcd.ie>
2023-03-19 21:25:09 +0000

    Progress on project page

--- frontend/src/pages/Project.js
+++ frontend/src/pages/Project.js
@@ -1,19 +1,27 @@
 import React from 'react'
+import React_Logo from './images/react_logo.png'
 
 function Project() {
   return (
-    <div style={{textAlign: "center"}} > 
-      <h1>Project page</h1>
-      <p>
-        patient centric Q&A Dashboard
-      </p>
+    <div style={{textAlign: "center", padding:"60px"}}> 
+      <h1>Find out more about our project!</h1>
       <br></br>
-      <p> we developed a tool to help aid with understanding the characteristics
-         of patients involved in clinical trials in a way that is 
-         intuitive and human-accessible. 
+      <p> 
+        This web application was built using JavaScript, the React library, Python, SQLite and
+        The Natural Language Toolkit (NLTK).
       </p>
-      <p>meet our team.</p>
+      <img
+        alt="React logo"
+        src={React_Logo}
+        style={{height: "112.5x", width: "150px"}}
+      />
+
+      <p>
+        <br></br>
+        <h3>Project Specifications</h3>
+      </p>
     </div>
+    
   )
 }
 
