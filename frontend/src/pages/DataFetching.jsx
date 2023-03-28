aabe1490a9c7d4eeca7ee83586d399ab3e8b5756
cpusch <conpusch@yahoo.com>
2023-03-28 03:53:36 +0100

    changed default data

--- frontend/src/pages/DataFetching.jsx
+++ frontend/src/pages/DataFetching.jsx
@@ -1,48 +1,48 @@
 import React from 'react';
 import axios from 'axios';
-import { useState, useEffect } from "react";    
+import { useState, useEffect } from "react";
 import { FaSearch } from 'react-icons/fa';
 
 
 
-var data = [{name: "isobel", value:10}, {name:"Cillian", value: 20}];
+var data = [{ "name": "60.0,65.714", "value": 1820.0 }, { "name": "65.714,71.429", "value": 1942.0 }, { "name": "71.429,77.143", "value": 1894.0 }, { "name": "77.143,82.857", "value": 1510.0 }, { "name": "82.857,88.571", "value": 1904.0 }, { "name": "88.571,94.286", "value": 1787.0 }, { "name": "94.286,100.0", "value": 1695.0 }];
 
 
 
 function DataFetching() {
-    const [dataPionts, setDataPionts ] = useState({})
-    const [query, setQuery ] = useState('')
-    const [updatedQuery, setUpdatedQuery ] = useState('')
+    const [dataPionts, setDataPionts] = useState({})
+    const [query, setQuery] = useState('')
+    const [updatedQuery, setUpdatedQuery] = useState('')
 
-    useEffect(() => { 
-            const getData = async () => {
+    useEffect(() => {
+        const getData = async () => {
 
-                data = await axios.get(`http://localhost/query/${updatedQuery}`)
-                setDataPionts(data.data)
-                console.log(data.data) 
-            };
-            getData();
-        }, [updatedQuery])
-
-        const handleKeyDown = (e) => {
-            if(e.key === 'Enter'){
-                console.log('do validate')
-                setUpdatedQuery(query);
-            }
+            data = await axios.get(`http://localhost/query/${updatedQuery}`)
+            setDataPionts(data.data)
+            console.log(data.data)
         };
-        const handleChange = (e) =>{
-            setQuery(e.target.value)
+        getData();
+    }, [updatedQuery])
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            console.log('do validate')
+            setUpdatedQuery(query);
         }
+    };
+    const handleChange = (e) => {
+        setQuery(e.target.value)
+    }
 
     return (
         <div>
-           <FaSearch />
-            <input 
+            <FaSearch />
+            <input
                 placeholder='search...'
                 type="text"
                 value={query}
-                onChange = {handleChange}    
-               onKeyDown ={handleKeyDown}   
+                onChange={handleChange}
+                onKeyDown={handleKeyDown}
             />
         </div>
 
