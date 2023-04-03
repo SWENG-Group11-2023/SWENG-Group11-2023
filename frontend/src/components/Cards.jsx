0fc096278c29e87bbb26078bec790ddfc697934f
Isobel Radford-Dodd <radfordi@tcd.ie>
2023-04-03 17:11:27 +0100

    metrics in cards

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -4,10 +4,25 @@ import Graph from '../pages/Graphs';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import DownloadButton from './DownloadCSV';
+import { DataFetching, updatedQuery, data } from "../pages/DataFetching";
+import  { useState } from 'react';
+
 
 const Cards = () => {
-  const percentage = 66;
+  const [Userdata, setData] = useState([
+        {average: '79.678'},
+        {median: '80.0'},
+        {max: '100.0'},
+        {min: '60.0'},
+        {range: '40.0'},
+        {standarddeviation: '11.596'}
+  ]);
 
+  const updateData = () => {
+    const newData = data.data.metrics;
+      console.log(data.data.metrics)
+    setData(newData);
+  };
   return (
   <div>
     <Stack direction ='row' spacing={2}>
@@ -18,30 +33,30 @@ const Cards = () => {
           </Typography>
           <Typography variant = 'body2' color='skyblue'>
             <div style={{ width: 100, height: 100 }}>
-             <CircularProgressbar value={66} text={`${percentage}%`} />
+             <CircularProgressbar value={66}/>
             </div>  
           </Typography>
         </CardContent>
         <CardActions>
-          <DownloadButton />
+          <DownloadButton/>
         </CardActions>
       </Card>
     </Box>
 
     <Box width = '30%'>
-            <Card>
+        <Card>
         <CardContent>
-          <Typography gutterBottom variant ='h5' component='div'>react
+          <Typography gutterBottom variant ='h5' component='div'> {Userdata.summary}
           </Typography>
           <Typography variant = 'body2' color='skyblue'>
           <div style={{ width: 100, height: 100 }}>
-            <CircularProgressbar value={34} text={`34%`}/>
+            average
+           <h1>{Userdata[0].average}</h1>
           </div>
-
           </Typography>
         </CardContent>
         <CardActions>
-          <Button size='small'>Download</Button>
+          <Button size='small' onClick={updateData}>update</Button>
           <Button size='small'>Learn more</Button>
         </CardActions>
       </Card>
@@ -53,11 +68,11 @@ const Cards = () => {
           <Typography gutterBottom variant ='h5' component='div'>react
           </Typography>
           <Typography variant = 'body2' color='skyblue'>
-            this is a better layout!
+          max
+           <h1>{Userdata[2].max}</h1>
           </Typography>
         </CardContent>
         <CardActions>
-          <Button size='small'>Download</Button>
           <Button size='small'>Learn more</Button>
         </CardActions>
       </Card>
