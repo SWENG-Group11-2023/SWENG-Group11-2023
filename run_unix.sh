f3e2cf851bcd861e70bd466f12cef26d98e039f6
Aj Pakulyte <pakulyta@tcd.ie>
2023-04-14 11:16:45 +0100

    Runs newest frontend (unix file)

--- run_unix.sh
+++ run_unix.sh
@@ -5,5 +5,5 @@ cd ../frontend
 docker build -t sweng_frontend .
 
 cd ..
-docker run -d -p 80:80 backend
-docker run -d -p 3000:3000 frontend
+docker run -d -p 80:80 sweng_backend
+docker run -d -p 3000:3000 sweng_frontend
